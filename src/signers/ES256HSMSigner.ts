55b19a538ba4bb605cd0eefb739b61b5d0156523
Jeff Shuo <jeffshuo@secuxtech.com>
2024-07-15 17:49:46 +0800

    converted Uint8 to hex

--- src/signers/ES256HSMSigner.ts
+++ src/signers/ES256HSMSigner.ts
@@ -9,38 +9,45 @@ import { sha256 } from '../Digest.js'
  *
  *  @example
  *  ```typescript
- *  const sign: Signer = ES256Signer(process.env.PUF_HSM_REMOTE_URL)
+ *  const sign: Signer = ES256HSMSigner(process.env.PUF_HSM_REMOTE_URL)
  *  const signature: string = await sign(data)
  *  ```
  *
  *  @param    {String}    PUF_HSM_REMOTE_URL   a puf hsm url as `Uint8Array`
  *  @return   {Function}               a configured signer function `(data: string | Uint8Array): Promise<string>`
  */
+
+function uint8ArrayToHex(uint8Array: Uint8Array) {
+  return Array.from(uint8Array, byte => byte.toString(16).padStart(2, '0')).join('');
+}
+
 export function ES256HSMSigner(pufHsmRemoteUrl: string): Signer {
   return async (data: string | Uint8Array): Promise<string> => {
     try {
-      const response = await fetch(pufHsmRemoteUrl+'/pufs_p256_sign_js', {
+      const hashArray = sha256(data);
+      const hashHex = uint8ArrayToHex(hashArray);
+
+      const response = await fetch(`${pufHsmRemoteUrl}/pufs_p256_sign_js`, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
+          'Content-Type': 'text/plain',
         },
-        body: JSON.stringify({ content: sha256(data) }),
+        body: hashHex,
+      });
+      if (!response.ok) {
+        const errorText = await response.text();
+        throw new Error(`HTTP error! status: ${response.status}, response: ${errorText}`);
+      }
+
+      const signature = await response.json();
+      return toJose({
+        r: leftpad(signature.sig.r.toString(16)),
+        s: leftpad(signature.sig.s.toString(16)),
       });
 
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-      const signature = await response.text()
-      return toJose({
-        r: leftpad(JSON.parse(signature).r.toString(16)),
-        s: leftpad(JSON.parse(signature).s.toString(16)),
-      })
-
     } catch (error) {
-      console.error("Error summarizing text:", error);
+      console.error("Error signing data:", error);
       throw error; // Handle the error appropriately
     }
-
-
   }
 }
